6109f9f11926d0723491958b87479fa6b66077e7
Aubrey Sledzinski <asledzinski88@gmail.com>
2022-10-29 21:21:42 -0400

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <Table />
+  </div>
+</template>
+
+<style>
+</style>
+<script>
+import Table from "../components/Table.vue";
+export default {
+  // eslint-disable-next-line vue/no-reserved-component-names
+  components: {Table}
+}
+</script>
